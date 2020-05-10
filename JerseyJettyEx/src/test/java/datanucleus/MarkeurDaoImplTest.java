517de6620bcf43155fd69a26501904192b6a739d
SaturninGANTET <saturnin.gantet@hotmail.fr>
2020-05-10 18:27:00 +0200

    Test DAO Marqueur

---
+++ JerseyJettyEx/src/test/java/datanucleus/MarkeurDaoImplTest.java
@@ -0,0 +1,35 @@
+package datanucleus;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import dao.Map;
+import dao.MapDao;
+import dao.Markeur;
+import dao.MarkeurDao;
+import dao.User;
+import dao.UserDao;
+import dao.dn.MapDaoImpl;
+import dao.dn.MarkeurDaoImpl;
+import dao.dn.UserDaoImpl;
+
+
+
+public class MarkeurDaoImplTest {
+
+	@Test
+	public void test() {
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("DataSource");
+		MarkeurDao markeurDao = new MarkeurDaoImpl(pmf);
+		Markeur mark = new Markeur();
+		mark.setLatitude(1.0);
+		mark.setLongitude(1.0);
+		markeurDao.addMarkeur(mark);
+		Markeur nouveau = markeurDao.getMarkeur(1.0, 1.0);
+		System.out.println(nouveau.getName());
+		Assert.assertNotNull(nouveau);
+	}
+}
